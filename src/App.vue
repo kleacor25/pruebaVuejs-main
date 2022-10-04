3d328b76f82bb07728044bf89d036ca5f3754e53
kleisy <kleacor25@gmail.com>
2022-10-04 00:32:11 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Tarea />
+</template>
+
+<script>
+import Tarea from './components/MyTarea.vue'
+                                                    
+
+export default {
+  name: 'App',
+  components: {
+    Tarea
+}
+}
+</script>
+
+<style>
+
+</style>
